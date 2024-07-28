247d15ee8fcd6bd76bed6168bc630dc6425f6b99
JacobMarineau <jacobmarineaump@gmail.com>
2024-07-28 16:55:47 -0500

    Finished

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -69,7 +69,7 @@ console.log(diceRoll);
 //      set `petStatus` to the value of "Oh no, I have too many pets!"
 let petStatus = "";
 {
-  if (pets < allowedPets) petStatus = "I can have more pets.";
+  if (pets < allowedPets) petStatus = "I can have more pets";
   else if (pets === allowedPets) petStatus = "I have enough pets.";
   else if (pets > allowedPets) petStatus = "Oh no, I have too many pets!";
 }
